a44fa61719c38f73aa55576b653b24a6c48118f9
Galina <gmonitor@student.21-school.ru>
2022-01-06 19:40:03 +0300

    Add lstindex and not-working execute

---
+++ execute.c
@@ -0,0 +1,75 @@
+#include "minishell.h"
+
+int main()
+{
+	t_arguments args;
+	args.pipes_count = 1;
+	args.commands_count = 2;
+
+
+	void *command1 = malloc(sizeof(t_command));
+	((t_command *)command1)->command = "ls";
+	((t_command *)command1)->args = NULL;
+	((t_command *)command1)->flags |= FLAG_PIPE_OUT;
+
+	void *command2 = malloc(sizeof(t_command));
+	((t_command *)command2)->command = "ls";
+	((t_command *)command2)->args = NULL;
+	((t_command *)command2)->flags |= FLAG_PIPE_IN;
+
+	t_list *commands = ft_lstnew(command1);
+	ft_lstadd_back(&commands, ft_lstnew(command2));
+
+
+	int fd[args.pipes_count][2];
+	int count = 0;
+	while (count < args.pipes_count)
+		if (pipe(fd[count++]) == -1)
+			return 1;
+
+
+	int count_commands = 0;
+	int count_pipes = 0;
+	int pid;
+	t_command *command;
+	while (count_commands < args.commands_count)
+	{
+		pid = fork();
+		if (pid < 0) {
+			return 2;
+		}
+
+		if (pid == 0)
+		{
+			// Child
+			command = (t_command *)ft_lstindex(commands, count)->content;
+			if (command)
+			{
+				if (command->flags & FLAG_PIPE_IN)
+					dup2(fd[count_pipes][0], STDIN_FILENO);
+				if (command->flags & FLAG_PIPE_OUT)
+					dup2(fd[count_pipes][1], STDOUT_FILENO);
+				while (count_pipes < args.pipes_count)
+				{
+					close(fd[count_pipes][0]);
+					close(fd[count_pipes][1]);
+					count_pipes++;
+				}
+				execlp(command->command, command->command, NULL);
+			}
+		}
+		count_commands++;
+		count_pipes = 0;
+	}
+
+	while (count_pipes < args.pipes_count)
+	{
+		close(fd[count_pipes][0]);
+		close(fd[count_pipes][1]);
+		count_pipes++;
+	}
+
+	wait(0);
+	wait(0);
+	return 0;
+}
--- minishell.h
+++ minishell.h
@@ -5,9 +5,25 @@
 #include <sys/wait.h>
 #include "libft/libft/libft.h"
 
+# define FLAG_PIPE_OUT		0b000001
+# define FLAG_STDOUT		0b000010
+# define FLAG_PIPE_IN		0b000100
+# define FLAG_STDIN			0b001000
+# define FLAG_REDIRECT_1	0b010000
+# define FLAG_REDIRECT_2	0b100000
+
 typedef struct		s_command
 {
 	char *command;
 	char *args;
 	int flags;
 } t_command;
+
+typedef struct		s_arguments
+{
+	int pipes_count;
+	int commands_count;
+} t_arguments;
+
+// Service
+t_list	*ft_lstindex(t_list *lst, int index);
